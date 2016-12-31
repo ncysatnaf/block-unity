09fa2d649ad3d9f6ddb7608cb1cafb6d8ddefc45
vai <ncysatnaf@gmail.com>
2016-12-31 14:40:16 +0800

    init

---
+++ inputManage/Assets/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Move(MoveDirection md)
+	{
+		Debug.Log(md.ToString() + " move.");
+	}
+}
