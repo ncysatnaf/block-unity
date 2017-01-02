b7d157d5185a7ecf592b2aeb849cf5d0c01c9226
vai <ncysatnaf@gmail.com>
2017-01-02 15:58:40 +0800

    RayClick

---
+++ RayClick/Assets/RayClick.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RayClick : MonoBehaviour 
+{
+
+    private Camera Cam = null;
+
+    void Awake() 
+    {
+        Cam = GetComponent<Camera>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray R = Cam.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
+
+            RaycastHit Hit;
+
+
+            if(Physics.Raycast(R, out Hit, Mathf.Infinity))
+            {
+                Debug.Log(Hit.collider.gameObject.name);
+            }
+        }
+	}
+}
