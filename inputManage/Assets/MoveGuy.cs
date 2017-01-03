da632a0c631b111a26af7f8e0d3d353440cc2ed1
vai <ncysatnaf@gmail.com>
2017-01-03 11:13:23 +0800

    move guy

---
+++ inputManage/Assets/MoveGuy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveGuy : MonoBehaviour {
+
+    public float mult = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float vecX = this.transform.position.x;
+        float vecZ = this.transform.position.z;
+        this.transform.position = new Vector3(vecX += Input.GetAxis("Horizontal")*mult, 1, vecZ += Input.GetAxis("Vertical")*mult);
+	}
+}
