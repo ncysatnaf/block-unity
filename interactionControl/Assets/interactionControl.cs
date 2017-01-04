94a8fb0f649f5ca9fdc43ef7d8214f082a337d26
vai <ncysatnaf@gmail.com>
2017-01-04 18:39:14 +0800

    interactionControl and animation

---
+++ interactionControl/Assets/interactionControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class interactionControl : MonoBehaviour {
+
+    private Animator anim;
+
+    void OnTriggerEnter(Collider obj)
+    {
+        Debug.Log("onTrigger");
+        anim.SetBool("isUp", true);
+    }
+
+    void OnTriggerExit(Collider obj)
+    {
+        Debug.Log("outTrigger");
+        anim.SetBool("isUp", false);
+    }
+
+	// Use this for initialization
+	void Start () {
+        anim = this.transform.parent.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
